afcf79c8db712fb1cb1da175e139bc07c32f2c0a
Artyom <yom@artyom.me>
2018-01-31 18:41:05 +0300

    Make HTML not broken

--- src/ToHTML.hs
+++ src/ToHTML.hs
@@ -1,6 +1,7 @@
 module ToHTML where
 
 import Data.Foldable as F (for_)
+import Data.Monoid
 import Data.Text
 import Data.Text.Lazy (toStrict)
 import Data.Time
@@ -9,23 +10,38 @@ import Lucid
 import qualified Text.MMark as MMark (render)
 
 qaSessionToHTML :: QaSession Person -> Text
-qaSessionToHTML = toStrict . renderText . hQaSession
+qaSessionToHTML = toStrict . renderText . hHtmlWrap . hQaSession
+
+hHtmlWrap :: Html () -> Html ()
+hHtmlWrap inner = do
+  head_ $ do
+    link_ [href_ "css.css", rel_ "stylesheet"]
+    link_ [href_ "qa.css", rel_ "stylesheet"]
+    link_ [href_ "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400", rel_ "stylesheet"]
+    link_ [href_ "https://fonts.googleapis.com/css?family=Overpass+Mono:300&amp;subset=latin-ext", rel_ "stylesheet"]
+    meta_ [charset_ "UTF-8"]
+  body_ inner
 
 hQaSession :: QaSession Person -> Html ()
-hQaSession QaSession{..} = (div_ `with` [class_ "qa-session"]) $ do
+hQaSession QaSession{..} = div_ [class_ "qa-session"] $ do
   hHeader qassTitle qassDate
   hConversation qassConversation
 
 hHeader :: Title -> Day -> Html ()
-hHeader Title{..} day = (div_ `with` [class_ "qa-header"]) $ do
-  (h2_ `with` [class_ "qa-title"]) (toHtml titleText)
+hHeader Title{..} day = div_ [class_ "qa-header"] $ do
+  h2_ [class_ "qa-title"] (toHtml titleText)
   let timeString = formatTime defaultTimeLocale "%Y-%m-%d" day
-  (time_ `with` [class_ "qa-time"]) (toHtmlRaw timeString)
+  time_ [class_ "qa-time"] (toHtmlRaw timeString)
 
 hConversation :: Conversation Person -> Html ()
 hConversation Conversation{..} =
-  F.for_ conversationMessages $ \Message{..} ->
-    (p_ `with` [class_ "qa-message"]) $ do
-      let Alias alias = pAlias msgAuthor
-      (span_ `with` [class_ "qa-author"]) (toHtml alias)
-      (span_ `with` [class_ "qa-message"]) (MMark.render msgContent)
+  F.for_ conversationMessages $ \Message{..} -> do
+    let Person{..} = msgAuthor
+    div_ [class_ ("qa-message " <> roleClass pRole)] $ do
+      let Alias alias = pAlias
+      p_ [class_ "qa-author"] (toHtml alias)
+      div_ [class_ "qa-content"] (MMark.render msgContent)
+
+roleClass :: Role -> Text
+roleClass Client     = "qa-role-client"
+roleClass Consultant = "qa-role-consultant"
